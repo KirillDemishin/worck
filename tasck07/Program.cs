f6a955c4b406767c8b644f5f92da081ed1cf4a3a
Kirill <kirilshou@gmail.com>
2023-08-01 20:45:53 +0800

    поставил пробел

--- tasck07/Program.cs
+++ tasck07/Program.cs
@@ -8,7 +8,7 @@
 Console.WriteLine("Введите трёхзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number < 0) number*=-1;
+if (number < 0) number *= -1;
 
 if (number >= 100 && number <= 999)
 {
